6e1be62bee1ca3d3fef0749756a3b22f45628e42
Sai-Sindhu-2607 <saisindhu306@gmail.com>
2024-08-12 22:38:52 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,13 @@
+x=int(input("Enter value 1:"))
+y=int(input("Enter value 2:"))
+Sum=x+y
+diff=x-y
+mult=x*y
+div=x/y
+mod=x%y
+print("Numbers are:",x,y)
+print("Sum=",Sum)
+print("Difference=",diff)
+print("product=",mult)
+print("Division=",div)
+print("module=",mod)

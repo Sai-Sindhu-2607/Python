4cd3c058cdf0ffcfb291676609778bc9324d65d3
Sai-Sindhu-2607 <saisindhu306@gmail.com>
2024-08-12 21:19:17 +0530

    Add files via upload

---
+++ biggestof three.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter a number:"))
+n2=int(input("Enter a number:"))
+n3=int(input("Enter a number:"))
+if(n1>n2& n1>n3):
+    print(n1)
+elif(n2>n3):
+    print(n2)
+else:
+    print(n3)
+                
